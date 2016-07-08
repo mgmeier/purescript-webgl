1ded5bb41399118b941092a62321aa193edf022a
jutaro <juergen.nicklisch@symbolian.net>
2016-07-08 19:09:59 +0200

    Fixed readPixel bug.

--- src/Graphics/WebGLFramebuffer.js
+++ src/Graphics/WebGLFramebuffer.js
@@ -12,6 +12,20 @@ exports.unbindFramebuffer_ = function(target)
 {return function()
  {gl.bindFramebuffer(target,null);};};
 
- exports.readPixels__ = function(x,y,width,height,format,type,pixels)
-         {return function()
-          { gl.readPixels(x,y,width,height,format,type,pixels);};};
+    exports.readPixels_ = function (x)
+    {return function(y)
+     {return function(width)
+      {return function(height)
+       {return function(format)
+        {return function(type)
+         {return function(pixels)
+          {return function()
+           {gl.readPixels(x,y,width,height,format,type,pixels);}
+           ;}
+          ;}
+         ;}
+        ;}
+       ;}
+      ;}
+     ;}
+    ;
